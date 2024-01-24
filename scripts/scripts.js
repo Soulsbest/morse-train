7a34d14c1b86697d4393cfce1ba2349dd90af99a
Soulsbest <48022646+Soulsbest@users.noreply.github.com>
2024-01-24 23:11:04 +0000

    Added framework for encoding backend

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,38 @@
+import { letters, words, sentences, letter_to_morse, morse_to_letter } from './morse.js';
+
 function toEncode() {
     window.location.href = "encode.html";
 }
 
 function toDecode() {
     window.location.href = "decode.html";
+}
+
+function toTitle() {
+  window.location.href = "index.html";
+}
+
+function chooseRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function newProblem(type) {
+  switch (type) {
+    case "letter":
+      return chooseRandom(letters);
+    case "word":
+      return chooseRandom(words);
+    case "sentence":
+      return chooseRandom(sentences);
+    default:
+      return "Error generating problem text: type of problem requested incorrect";
+  }
+}
+
+function decode(char) {
+  return letter_to_morse[char];
+}
+
+function encode(char) {
+  return morse_to_letter[char];
 }
