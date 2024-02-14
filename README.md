57d25c927e4b425a8ca8820051ab20809e4c715a
Soulbest <owensoulbeast@gmail.com>
2024-02-13 20:14:21 -0800

    testing redirect error

--- README.md
+++ README.md
@@ -4,9 +4,3 @@
 A simple morse code trainer for people who are poggers
 
 heavily wip
-
-
-### This file was edited on my home computer (but wait, it was created on my chromebook)
-This project is the first time that I have used github
-
-I am a buffon matryoshkaed instead of a nincomp and an imbecile
--- constants.js
+++ constants.js
@@ -30,20 +30,20 @@ const letter_to_morse = {
 const morse_to_letter = {};
 for (key in letter_to_morse) {
   morse_to_letter[letter_to_morse[key]] = key;
-}
+};
 
 const letters = [
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
   "t", "u", "v", "w", "x", "y", "z"
-]
+];
 
 const words = [
   "plums", "banana", "apple", "orange", "pear", "peach", "grape", "kiwi", "theseareallofthefruitsitautofilled"
-]
+];
 
 const sentences = [
   "I have developed one single sentence, however this is not a problem as my sentence is perfet",
   "this is my other sentence );"
-]
+];
 
-export {letter_to_morse, morse_to_letter, letters, words, sentences}
+export {letter_to_morse, morse_to_letter, letters, words, sentences};
